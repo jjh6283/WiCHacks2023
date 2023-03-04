64eb2fb81bf984541e149733a5c852239aa8f84c
Mildness Onyekwere <mno5865@rit.edu>
2023-03-04 16:31:39 -0500

    initialized mealplan page

---
+++ gAIn/src/app/mealplan/mealplan.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mealplan',
+  templateUrl: './mealplan.component.html',
+  styleUrls: ['./mealplan.component.css']
+})
+export class MealplanComponent {
+
+}
